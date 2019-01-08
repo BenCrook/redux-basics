c5eb601c206912ef9f017bad29fad171ae21b294
Ben Crook <ben@space48.com>
2019-01-08 13:11:08 +0000

    Rename App.test.js to app.test.js for consistency

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from "../redux/reducers";
-import App from './App';
+import App from './app';
 
 const store = createStore(reducer);
 
